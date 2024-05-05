6f23b0630a2cd763774466270b74288dad560a89
Ben Hilger <bhilger78@gmail.com>
2024-05-05 17:03:17 -0400

    add main go file for variables

---
+++ go-aws/variables/main.go
@@ -0,0 +1,24 @@
+package variables
+
+import "fmt"
+
+func Main() {
+	var myName string = "ben"
+
+	fmt.Printf("Hello my name is %s\n", myName)
+
+	// Inferred Type
+	myNameInferred := "ben"
+	myInt := 10
+	myFloat := 10.0
+	fmt.Printf("Hello my name is %s, my int is %d, my float is %f\n", myNameInferred, myInt, myFloat)
+
+	// Defaults
+	var myFriendName string
+	var myBool bool
+	var myOtherInt int
+
+	myFriendName = "Prime"
+
+	fmt.Printf("my other friends name %s my bool %t my other int %d\n", myFriendName, myBool, myOtherInt)
+}
